ebe69a75cb3404d46547232b841c149dd1e0e388
mappy12 <mappydesuu@gmail.com>
2025-09-24 16:03:33 +0400

    Небольшой рефакторинг кода

--- lab1/main.py
+++ lab1/main.py
@@ -1,19 +1,17 @@
 import numpy as np
 
 
-def main():
-    print("###########Задание 1##########", "\n")
-
+def task_1():
     print("Матрица 10х10 из вещественных единиц:", "\n")
-    a = np.ones((10,10), dtype=float)
+    a = np.ones((10, 10), dtype=float)
     print(a, "\n")
 
     print("Единичная матрица 10х10:", "\n")
     b = np.identity(10, dtype=int)
     print(b, "\n")
 
-    print("###########Задание 2##########", "\n")
 
+def task_2():
     mtrx = np.array([[2, 1, 3, 6],
                      [4, 1, 3, 3],
                      [5, 2, 4, 1],
@@ -24,6 +22,25 @@ def main():
     print("Матрица:", "\n", "\n", mtrx, "\n")
     print("Определитель: ", det, "\n")
     print(f"Определитель (в целочисленном виде): {int(det)}")
+
+
+def task_3():
+
+
+
+def main():
+    print("###########Задание 1##########", "\n")
+
+    task_1()
+
+    print("###########Задание 2##########", "\n")
+
+    task_2()
+
+    print("###########Задание 3##########", "\n")
+
+    task_3()
+
 
 if __name__ == "__main__":
     main()
