a75f63181861bf37a4c6833ecced0cdce8359572
mappy12 <mappydesuu@gmail.com>
2025-09-24 21:18:10 +0400

    Задание 4,5

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import integrate
 
 
 def task_1():
@@ -50,6 +51,25 @@ def task_3():
         print("Система не имеет единственного решения!")
 
 
+def task_4():
+    try:
+        result, error = integrate.quad(lambda x: 1 / (1 - x**2) ** 0.5, -0.5, 0.5)
+        print(f"∫_{-1/2}^{1/2} dx/√(1-x^2) = {result}")
+
+    except Exception as e:
+        print(f"Интеграл расходится: {e}")
+
+
+def task_5():
+    try:
+        result, error = integrate.quad(lambda x: np.cos(2*x), 0, np.inf)
+        print(f"∫_0^(inf) cos(2x) dx = {result}")
+
+    except Exception as e:
+        print(f"Интеграл расходится: {e}")
+
+def task_6():
+
 
 def main():
     print("###########Задание 1##########", "\n")
@@ -64,7 +84,17 @@ def main():
 
     task_3()
 
+    print("###########Задание 4##########", "\n")
 
+    task_4()
+
+    print("###########Задание 4##########", "\n")
+
+    task_5()
+
+    print("###########Задание 6##########", "\n")
+
+    task_6()
 
 if __name__ == "__main__":
     main()
